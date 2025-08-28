8733956c903f987f4ae0d7cce9e85a8d47c70eb1
Adithya N <nadithya666@gmail.com>
2025-08-28 23:42:12 +0530

    3Sum

---
+++ onArray/medium/3Sum/driver_code.py
@@ -0,0 +1,20 @@
+from threesum import Solution
+import json
+
+with open("test_cases.json") as f:
+    test_cases = json.load(f)
+
+for i, case in enumerate(test_cases, 1):
+    result = Solution().threeSum(case["input"]["nums"])
+    if result != case["output"]:
+        print("Failed Test case 😔")
+        print("Input: ", case["input"])
+        print("Expected: ", case["output"])
+        print("Got: ", result)
+        print()
+    else:
+        print("Test case passed! 😎")
+        print("Input: ", case["input"])
+        print("Expected: ", case["output"])
+        print("Got: ", result)
+        print()
