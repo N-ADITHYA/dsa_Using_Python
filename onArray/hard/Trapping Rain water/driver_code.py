8274c13ae44c8cecc67c676294cf5fe616183265
Adithya N <nadithya666@gmail.com>
2025-09-06 21:15:39 +0530

    Optimized for hard

--- onArray/hard/Trapping Rain water/driver_code.py
+++ onArray/hard/Trapping Rain water/driver_code.py
@@ -1,5 +1,7 @@
 import json
 from trap_Normal_Soln import Solution
+from trap_Optimized_Soln import Solution as d
+
 
 with open("test_cases.json") as f:
     test_cases = json.load(f)
@@ -7,17 +9,37 @@ with open("test_cases.json") as f:
 
 for i, case in enumerate(test_cases, 1):
     result = Solution().trap(case["input"]["height"])
+    resultO = d().trapO(case["input"]["height"])
 
 
     if result != case["output"]:
+        print("Normal Soln")
         print("Oh noooo, It's failed 😔")
         print("Input: ", case["input"]["height"])
         print("Expected: ", case["output"])
         print("Got: ", result)
         print()
     else:
+        print("Normal Soln")
         print("Oh Yeahhhh, It's passed 😎")
         print("Input: ", case["input"]["height"])
         print("Expected: ", case["output"])
         print("Got: ", result)
+        print()
+
+for i, case in enumerate(test_cases, 1):
+    resultO = d().trapO(case["input"]["height"])
+    if resultO != case["output"]:
+        print("For optimized Soln")
+        print("Oh noooo, It's failed 😔")
+        print("Input: ", case["input"]["height"])
+        print("Expected: ", case["output"])
+        print("Got: ", resultO)
+        print()
+    else:
+        print("For optimized Soln")
+        print("Oh Yeahhhh, It's passed 😎")
+        print("Input: ", case["input"]["height"])
+        print("Expected: ", case["output"])
+        print("Got: ", resultO)
         print()
