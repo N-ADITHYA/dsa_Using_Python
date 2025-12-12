bdb25f84607a2ba1d5f3deda568e3877d1cda2cc
N-ADITHYA <nadithya635@gmail.com>
2025-12-12 10:36:31 +0530

    Hmm code added

---
+++ Quests/Linked_List/reverseLinkedList_Easy/reverseList.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+class Solution:
+    def reverseList(self, head: Optional[Node]) -> Optional[Node]:
+        curr = head
+        prev = None
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
