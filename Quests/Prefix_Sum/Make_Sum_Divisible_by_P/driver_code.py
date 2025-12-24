a07052608a0f2fc123e44611f817c4480afff53d
N-ADITHYA <nadithya635@gmail.com>
2025-12-24 22:42:23 +0530

    Can't delay it anymore

--- Quests/Prefix_Sum/Make_Sum_Divisible_by_P/driver_code.py
+++ Quests/Prefix_Sum/Make_Sum_Divisible_by_P/driver_code.py
@@ -1,1 +1,20 @@
 from typing import List
+from minSubarray import Solution
+import json
+with open("test_cases.json") as f:
+    test_cases = json.load(f)
+print(test_cases)
+for test in test_cases:
+    inp = test["nums"]
+    p = test["p"]
+    expected = test["expected"]
+    sol = Solution()
+    output = sol.minSubarray(inp, p)
+
+    if output == expected:
+        print("Done")
+    else:
+        print("Nope")
+        print("Input is: ", inp)
+        print("Output is: ", output)
+        print("Expected is: ", expected)
